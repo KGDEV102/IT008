665f0d5ba23dbf53f4b721d8f883bde4f8bd5037
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-10-17 20:45:32 +0700

    Add KhuDat class inheriting from BDS

---
+++ BTTH2/BAI5/KhuDat.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_5
+{
+    internal class KhuDat:BDS
+    {
+        public override int getLoai()
+        {
+            return 0;
+        }
+    }
+
+}
